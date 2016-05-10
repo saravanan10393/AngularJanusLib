f83b60021fa64f7f21a6bc1febbcc748429e96f2
saravanan <saravanan.10393@gmail.com>
2016-05-10 18:58:59 +0530

    comment update

--- app/js/service/videoFactory.js
+++ app/js/service/videoFactory.js
@@ -9,6 +9,7 @@
     function VideoCall($http) {
         var service = {
             call:call,
+            pickCall : pickCall,
             addToCall : addToCall,
             muteAudio : muteAudio,
             unMuteAudio : unMuteAudio,
@@ -19,13 +20,31 @@
         
         return service;
 
+        //initilize the januz when factory instantiate and create a session
+        
+        
+
         ////////////////
-        function call() {
-            
+        function call(toWhom) {
+            //generate a roomId hint : use roomId as group for recording
+            // use already existing message way..
+            //publish his own stream
+            // create a janus offer to start attach to gateway
+            // and send socket message to concerned user to raise call popup;     
          }
         
         ///////////////
-        function addToCall() { }
+        function pickCall(roomId) {
+            // publish his own stream on given roomId
+            // create an offer to given particular roomId
+        }
+        
+        ///////////////
+        function addToCall(member) {
+            //create a group in db for messages for joined 3 people
+            //simply send socket message to user to show popup to join in this call. //raise waring if he already in another call
+            //when he pick up call publish his own stream to current call roomId
+        }
         
         ///////////////
         function muteAudio() { }
