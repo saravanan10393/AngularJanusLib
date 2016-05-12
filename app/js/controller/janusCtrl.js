3b834493f241181146bc0880fbccbc162dbfc629
saravanan <saravanan.10393@gmail.com>
2016-05-12 18:33:08 +0530

    update

--- app/js/controller/janusCtrl.js
+++ app/js/controller/janusCtrl.js
@@ -5,9 +5,39 @@
         .module('janus')
         .controller('videoCallCtrl', videoCallCtrl);
 
-    videoCallCtrl.$inject = ['$scope'];
-    function videoCallCtrl($scope) {
-        var vm = this;
+    videoCallCtrl.$inject = ['$scope','VideoCall'];
+    function videoCallCtrl($scope,VideoCall) {
+        var vm = this,roomId = 12345;
+        vm.currentUser = null;
         
+        vm.call = function(to){
+            VideoCall.call(currentUser.name);
+            socket.emit('call:outgoing',roomId);
+        };
+        
+        vm.cut = function(){
+            VideoCall.cut();
+        }
+        
+        vm.toggleAudio = function(){
+            VideoCall.toggleAudio(function(status){
+               //change ui; 
+            });
+        };
+        
+        vm.toggleVideo = function(){
+            VideoCall.toggleVideo(function(status){
+               //change ui; 
+            });
+        };
+        
+        socket.on('call:incomming',function(roomId){
+           if(VideoCall.isBusy){
+               socket.emit('user:busy',"userBusy");
+               return;
+           }
+           console.log('call is comming and auto attending call to roomId ',roomId);
+           VideoCall.pickCall(roomId);
+        });
     }
 })();
